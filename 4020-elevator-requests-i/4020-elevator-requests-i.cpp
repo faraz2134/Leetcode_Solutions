52e4b2412531ad56c8c8713b08cde88514843cfb
Md Akmal Faraz <akmalfarazmd@gmail.com>
2026-09-03 07:20:44 +0530

    Time: 0 ms (100%), Space: 30.9 MB (55.79%) - LeetHub

---
+++ 4020-elevator-requests-i/4020-elevator-requests-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int elevatorRequests(int n, vector<int>& requests) {
+        int x=0;
+        int sum=0;
+        for(int i=0;i<requests.size();i++){
+            sum+=abs(requests[i]-x);
+            x=requests[i];
+        }
+        return sum;
+        
+    }
+};
